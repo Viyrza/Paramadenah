ad442ffdabeb38c9ef454837ac439f5e93dff9e1
RayhanAlsauqi <rayhan.alsauqi@sarmurni.co.id>
2024-12-14 00:58:07 +0700

    feat(tablist): adding tablist for auth page

---
+++ src/app/auth/page.js
@@ -0,0 +1,76 @@
+'use client';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+// import Logo from '@/png/kepala';
+import Image from 'next/image';
+import Logo from '@/png/logoapk2.png';
+import Kepala from '@/png/kepala.png';
+import Email from '@/png/email.png';
+import Gembok from '@/png/gembok.png';
+import Blind from '@/png/Blind.png';
+import apel from '@/png/apel.png';
+import gugel from '@/png/Google.png';
+
+import { useState } from 'react';
+import { Signin } from '@/components/containers/sign-in/page';
+import { SignUp } from '@/components/containers/sign-up/page';
+
+export default function Page() {
+  const [activeTab, setActiveTab] = useState(0);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+  return (
+    <Tabs selectedIndex={activeTab} onSelect={(index) => setActiveTab(index)}>
+      <div className="h-screen bg-background_secondary">
+        <div className="bg-background_primary shadow-lg w-full max-w-sm">
+          <div className="text-center">
+            <div className="flex justify-center mb-4">
+              <Image
+                src={Logo}
+                alt="Paramadenah Logo"
+                width={352}
+                height={99}
+              />
+            </div>
+            <p className="text-white font-bold mb-4">
+              Selamat datang di Paramadenah
+            </p>
+            <p className="text-white">
+              Silahkan lakukan Sign up atau Login untuk mengoperasikan
+              Paramadenah
+            </p>
+          </div>
+          <TabList>
+            <div className="flex justify-center gap-2">
+              <Tab
+                className={`px-4 py-2 w-full text-center  ${
+                  activeTab === 0 ? 'border-b-2 border-[#00304D]' : ''
+                } focus:outline-none`}
+              >
+                Login
+              </Tab>
+              <Tab
+                className={`px-4 py-2 w-full text-center ${
+                  activeTab === 1 ? 'border-b-2 border-[#00304D]' : ''
+                } focus:outline-none`}
+              >
+                SIGN UP
+              </Tab>
+            </div>
+          </TabList>
+        </div>
+        <TabPanel>
+          <Signin
+            isPasswordVisible={isPasswordVisible}
+            togglePasswordVisibility={togglePasswordVisibility}
+          />
+        </TabPanel>
+        <TabPanel>
+          <SignUp />
+        </TabPanel>
+      </div>
+    </Tabs>
+  );
+}
---
+++ src/components/containers/sign-up/page.js
@@ -0,0 +1,106 @@
+import Image from 'next/image';
+import Logo from '@/png/logoapk2.png';
+import Kepala from '@/png/kepala.png';
+import Email from '@/png/email.png';
+import Gembok from '@/png/gembok.png';
+
+export function SignUp() {
+  return (
+    <form className="mt-6 bg-background_secondary w-full p-6">
+      <div className="border bg-white rounded-lg mb-4 flex items-center">
+        <div className="p-2">
+          <Image
+            src={Kepala}
+            alt="Kepala"
+            width={30}
+            height={30}
+            priority={true}
+            quality={100}
+            className="justify-center align-middle"
+          />
+        </div>
+        <input
+          id="name"
+          type="text"
+          placeholder="Nama Anda"
+          className="w-full left-14 py-2 bg-transparent text-black focus:outline-none"
+        />
+      </div>
+
+      <div className="border bg-white rounded-lg mb-4 flex items-center">
+        <div className="p-2">
+          <Image
+            src={Email}
+            alt="Email"
+            width={30}
+            height={30}
+            priority={true}
+            quality={100}
+            className="justify-center align-middle"
+          />
+        </div>
+        <input
+          id="email"
+          type="email"
+          placeholder="Masukkan email Anda"
+          className="w-full left-14 py-2 bg-transparent text-black focus:outline-none"
+        />
+      </div>
+
+      <div className="border bg-white rounded-lg mb-4 flex items-center">
+        <div className="p-2">
+          <Image
+            src={Gembok}
+            alt="Gembok"
+            width={30}
+            height={30}
+            priority={true}
+            quality={100}
+            className="justify-center align-middle"
+          />
+        </div>
+        <input
+          id="password"
+          type="password"
+          placeholder="Masukkan sandi Anda"
+          className="w-full left-14 py-2 bg-transparent text-black focus:outline-none"
+        />
+      </div>
+
+      <div className="border bg-white rounded-lg mb-4 flex items-center">
+        <div className="p-2">
+          <Image
+            src={Gembok}
+            alt="Gembok"
+            width={30}
+            height={30}
+            priority={true}
+            quality={100}
+            className="justify-center align-middle"
+          />
+        </div>
+        <input
+          id="confirm-password"
+          type="password"
+          placeholder="Konfirmasikan sandi Anda"
+          className="w-full left-14 py-2 bg-transparent text-black focus:outline-none"
+        />
+      </div>
+
+      <p className="text-black text-sm text-center mt-2 mb-4">
+        Dengan membuat akun ini, anda menyetujui{' '}
+        <a href="#" className="text-black underline">
+          Syarat & Ketentuan
+        </a>{' '}
+        yang berlaku
+      </p>
+
+      <button
+        type="submit"
+        className="bg-blue-900 text-white text-center w-full py-2 rounded-lg hover:bg-blue-800"
+      >
+        Sign up
+      </button>
+    </form>
+  );
+}
